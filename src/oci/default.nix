da2e3567445f149a166e2e060e5095935c68b0a0
usrbinkat <usrbinkat@braincraft.io>
2025-12-02 23:05:30 -0800

    fix(nix): pre-trust konductor flake and cachix substituters
    Configure nix.conf to eliminate interactive prompts when using flakes:
    - Set accept-flake-config=true for automatic flake config acceptance
    - Add trusted-users for kc2, kc2admin, and root
    - Use substituters/trusted-substituters instead of extra-* variants
    - Include both cache.nixos.org and nix-community.cachix.org

--- src/oci/default.nix
+++ src/oci/default.nix
@@ -173,8 +173,11 @@ let
     experimental-features = nix-command flakes
     sandbox = false
     filter-syscalls = false
-    extra-substituters = https://nix-community.cachix.org
-    extra-trusted-public-keys = nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+    accept-flake-config = true
+    trusted-users = root kc2 kc2admin
+    substituters = https://cache.nixos.org https://nix-community.cachix.org
+    trusted-substituters = https://cache.nixos.org https://nix-community.cachix.org
+    trusted-public-keys = cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
   '';
 
   # Pre-configured flake registry
